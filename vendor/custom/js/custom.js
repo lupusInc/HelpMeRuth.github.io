0fd4c2b28f9d7a25486f3d78dc832f6554fc310f
HelpMeRuth <jukeboxruthger1@gmail.com>
2017-11-27 19:29:27 +0100

    Page scrolling

--- vendor/custom/js/custom.js
+++ vendor/custom/js/custom.js
@@ -1,17 +1,20 @@
-//
+// Event functions
 function OnLoad() {
   console.log("Loaded"); //event log
   $(".contentblock").css("height", $(document).height());
+  countPages()
+  resetPages();
 }
 
 function Scroll() {
   console.log("Scrolled"); //event log
+  // Height can change on mobile whilst scrolling e.g chrome
+  scalePage();
 }
 
 function Resize() {
   console.log("Resized"); //event log
-  $(".contentblock").css("height", $(window).height()); // Reset the height
-  $(".contentblock").css("height", $(document).height()); // Set the actual height
+  scalePage();
 }
 // Events
 //
@@ -27,5 +30,49 @@ $(window).scroll(function() {
 $(window).resize(function() {
   Resize();
 });
+//
+//Functions
+var currentPage = 0;
+var pages = 0;
 
-function slide() {}
+function countPages() {
+  pages = 0;
+  for (var found = false; !found;) {
+    if ($(".page" + pages).length == 1) {
+      pages++;
+    } else {
+      found = true;
+      pages--;
+    }
+  }
+}
+
+function slide(newPage, down) {
+  if (down) {
+    $(".page" + currentPage).css("top", -$(document).height() + "px");
+  } else {
+    $(".page" + currentPage).css("top", $(document).height() + "px");
+  }
+  $(".page" + currentPage).css("position", "fixed");
+  $(".page" + newPage).css("top", "0px");
+  setTimeout(function() {
+    $(".page" + newPage).css("position", "initial");
+  }, 1000);
+  currentPage = newPage;
+}
+
+function resetPages() {
+  for (var i = 0; i <= pages; i++) {
+    if (i == currentPage) {
+      j = 0;
+    } else {
+      j = $(document).height();
+    }
+    $(".page" + i).css("top", j + "px");
+  }
+}
+
+function scalePage() {
+  $(".contentblock").css("height", $(window).height()); // Reset the height
+  $(".contentblock").css("height", $(document).height()); // Set the actual height
+}
